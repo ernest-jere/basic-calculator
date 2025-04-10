7370e2b3c8fdeff88b414a27c32459fb333a1ebb
Ernest Ernest <130362988+ernest-jere@users.noreply.github.com>
2025-04-10 11:40:39 +0200

    Update script.js
    Added line to cancel function
        expression[0] === "-" ? expression.unshift(0) : expression; // Check if the expression starts with a negative to add zero at the beginning of the expression

--- scripts/script.js
+++ scripts/script.js
@@ -139,6 +139,7 @@ function cancelKey(){
     topDisplay.textContent = topDisplay.textContent.slice(0, -1); 
     const string = topDisplay.textContent;
     const expression = string.split("");
+    expression[0] === "-" ? expression.unshift(0) : expression; // Check if the expression starts with a negative to add zero at the beginning of the expression
     for (let i = 0; i < expression.length; i++) {
       if ( expression [i] === "+" 
         || expression [i] === "-" 
